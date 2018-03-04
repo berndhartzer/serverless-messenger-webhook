c178b8716bc4914a6a063c85b793f6de9ed47db1
Bernd <berndhartzer@gmail.com>
2018-03-04 18:16:20 +1000

    updated post handler

--- handler/webhook-post.js
+++ handler/webhook-post.js
@@ -4,12 +4,38 @@ module.exports.handler = (event, context, callback) => {
 
   console.log(JSON.stringify(event));
 
-  callback(
-    null,
-    {
-      statusCode: 200,
-      body: 'ok',
-    }
-  );
+  const body = JSON.parse(event.body);
+
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(entry => {
+
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+    });
+
+    callback(
+      null,
+      {
+        statusCode: 200,
+        body: 'EVENT_RECEIVED',
+      }
+    );
+
+  } else {
+
+    // Return a '404 Not Found' if event is not from a page subscription
+    callback(
+      null,
+      {
+        statusCode: 404,
+      }
+    );
+
+  }
 
 }
