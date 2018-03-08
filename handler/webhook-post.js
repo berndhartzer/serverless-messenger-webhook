a9eb455994fb5b6160184f3b7905e52189d377fa
Bernd <berndhartzer@gmail.com>
2018-03-08 20:29:31 +1000

    cleaned up code styling

--- handler/webhook-post.js
+++ handler/webhook-post.js
@@ -31,7 +31,6 @@ module.exports.handler = (event, context, callback) => {
         handlePostback(sender_psid, webhook_event.postback);
       }
 
-
     });
 
     callback(
@@ -68,7 +67,7 @@ function handleMessage(sender_psid, received_message) {
 
     // Create the payload for a basic text message
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      'text': `You sent the message: '${received_message.text}'. Now send me an image!`
     }
   }
 
@@ -89,17 +88,19 @@ function callSendAPI(sender_psid, response) {
 
   // Construct the message body
   let request_body = {
-    "recipient": {
-      "id": sender_psid
+    recipient: {
+      id: sender_psid
     },
-    "message": response
+    message: response
   };
 
   let options = {
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      'access_token': PAGE_ACCESS_TOKEN
+    },
+    method: 'POST',
+    json: request_body
   };
 
   rp(options)
